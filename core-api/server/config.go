d619fcbe7a58a21beeb91a21721f66fa06218bf9
Andrew <andrey777-novikov777@yandex.ru>
2026-04-15 20:05:09 +0300

    Dockerized application and created compose.yaml

--- core-api/server/config.go
+++ core-api/server/config.go
@@ -1,9 +1,31 @@
 package server
 
-import "github.com/caarlos0/env/v11"
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
 
 type Config struct {
-	Port int `env:"CORE_API_PORT" envDefault:"8000"`
+	Port             int    `env:"CORE_API_PORT" envDefault:"8000"`
+	PostgresHost     string `env:"POSTGRES_HOST" envDefault:"localhost"`
+	PostgresPort     int    `env:"POSTGRES_PORT" envDefault:"5432"`
+	PostgresUser     string `env:"POSTGRES_USER"`
+	PostgresPassword string `env:"POSTGRES_PASSWORD"`
+	PostgresDB       string `env:"POSTGRES_DB"`
+	PostgresSSLMode  string `env:"POSTGRES_SSLMODE" envDefault:"disable"`
+}
+
+func (c *Config) DatabaseURL() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		c.PostgresUser,
+		c.PostgresPassword,
+		c.PostgresHost,
+		c.PostgresPort,
+		c.PostgresDB,
+		c.PostgresSSLMode,
+	)
 }
 
 func NewConfig() (*Config, error) {
