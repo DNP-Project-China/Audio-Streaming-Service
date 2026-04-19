b4c6c534b51dbab0006e7047a66c13e8b3024619
I-Leonid-I <gunkoleonid06@gmail.com>
2026-04-20 01:19:55 +0800

    s3_key updated to correspond API Structure

--- audio-converter/main.py
+++ audio-converter/main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import tempfile
+from uuid import UUID
 from dotenv import load_dotenv
 from kafka import KafkaConsumer
 
@@ -22,10 +23,12 @@ def main():
 
     for message in consumer:
         job = message.value
-        track_id = job.get('track_id')
-        s3_key = job.get('s3_key')
+        job_id = job.get('job_id')
+        track_id = UUID(job.get('track_id'))
+        path = job.get('path')
+        priority = job.get('priority', 0)
 
-        print(f"\n--- New Task: Track ID = {track_id} ---")
+        print(f"\n--- New Task: Job ID = {job_id}, Track ID = {track_id}, Priority = {priority} ---")
 
         current_status = get_track_status(track_id)
         if current_status in ['processing', 'ready']:
@@ -38,20 +41,23 @@ def main():
             local_mp3 = os.path.join(tmpdir, "raw.mp3")
             hls_output_dir = os.path.join(tmpdir, "hls")
 
-            if not s3_client.download_file(s3_key, local_mp3):
+            if not s3_client.download_file(path, local_mp3):
                 update_track_status(track_id, 'failed')
+                print(f"[FAILED] {job_id}: Failed to download file")
                 continue
 
             if not convert_audio_to_hls(local_mp3, hls_output_dir):
                 update_track_status(track_id, 'failed')
+                print(f"[FAILED] {job_id}: Failed to convert audio")
                 continue
 
             if not s3_client.upload_hls_folder(track_id, hls_output_dir):
                 update_track_status(track_id, 'failed')
+                print(f"[FAILED] {job_id}: Failed to upload HLS files")
                 continue
 
         update_track_status(track_id, 'ready')
-        print(f"--- Task for track {track_id} completed successfully! ---\n")
+        print(f"[SUCCESS] {job_id}: Task completed successfully ---\n")
 
 if __name__ == "__main__":
     main()
